18eebb9190535fa4fabe41dfe04a22925b6d7237
PatrickTheNightOwl <myfamilyisatreasure@gmail.com>
2026-08-02 12:04:27 +0700

    Subject Management v1.1

--- JS/subject-management.js
+++ JS/subject-management.js
@@ -3,13 +3,16 @@ const database = JSON.parse(localStorage.getItem("studyPlannerDatabase"));
 const currentUserId = Number(localStorage.getItem("currentUserId"));
 const currentUser = database.users.find((user) => user.id === currentUserId);
 const subjectsList = currentUser.subjects;
+const tasksList = currentUser.tasks;
 
 // Elements Retrieve
 const mainContainer = document.getElementById("main-content");
+const subjectsContainer = document.getElementById("subjects-container");
 const addSubjectButton = document.getElementById("add-subject");
 const subjectModal = document.getElementById("modal");
 const subjectModalOverlay = document.getElementById("modal-overlay");
 const closeModalButton = document.getElementById("cancel-btn");
+const deleteSubjectButton = document.getElementById("del-btn");
 const saveNewSubjectButton = document.getElementById("save-btn");
 const subjectName = document.getElementById("subject-name");
 const colorContainer = document.getElementById("color-recommendation");
@@ -17,6 +20,30 @@ const colorList = colorContainer.children;
 
 // Variable
 let selectedColor = "#9ca3af"; // default, if changed -> change
+
+// Render Function
+function renderSubjects() {
+  let html = "";
+
+  subjectsList.forEach((subject) => {
+    const remainingTasks = tasksList.filter(
+      (task) => task.subjectId === subject.id
+    ).length;
+    html += `
+            <div
+                class="subject-card"
+                style="background-color:${subject.color}">
+                <h3>${subject.name}</h3>
+                <p>${remainingTasks} task${
+      remainingTasks !== 1 ? "s" : ""
+    } remaining</p>
+            </div>
+        `;
+  });
+
+  subjectsContainer.innerHTML = html;
+}
+renderSubjects();
 
 // Functions and Events
 for (let color of colorList) {
@@ -30,6 +57,13 @@ addSubjectButton.addEventListener("click", () => {
 closeModalButton.addEventListener("click", () => {
   subjectModalOverlay.classList.add("hidden");
 });
+deleteSubjectButton.addEventListener("click", () => {
+  const name = subjectName.value;
+  subjectsList.filter((subject) => subject.name !== name);
+  alert("Subject deleted!");
+  localStorage.setItem("studyPlannerDatabase", JSON.stringify(database));
+  renderSubjects();
+});
 saveNewSubjectButton.addEventListener("click", () => {
   let id;
   if (subjectsList.length === 0) {
@@ -42,6 +76,14 @@ saveNewSubjectButton.addEventListener("click", () => {
   if (name.trim().length === 0) {
     alert("Please enter subject's name!");
     return;
+  } else if (
+    subjectsList.some(
+      (subject) =>
+        subject.name.trim().toLowerCase() === name.trim().toLowerCase()
+    )
+  ) {
+    alert("Subject's name already existed!");
+    return;
   } else {
     const newSubject = {
       id: id,
@@ -51,5 +93,6 @@ saveNewSubjectButton.addEventListener("click", () => {
     subjectsList.push(newSubject);
     alert("Subject created!");
     localStorage.setItem("studyPlannerDatabase", JSON.stringify(database));
+    renderSubjects();
   }
 });
